1ea7751967a586b487cf346a9077b2eb6a6f5a41
Lasaro Dumer <lasaro.dumer@gmail.com>
2017-11-01 20:30:06 -0200

    More test cases for Movement

--- CTServer/src/test/java/chungtoi/server/tests/TwoStepMovementTest.java
+++ CTServer/src/test/java/chungtoi/server/tests/TwoStepMovementTest.java
@@ -5,15 +5,15 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 import chungtoi.server.Movement;
 
-public class OneStepMovementTest extends TestCase{
-    private int movement = 1;
+public class TwoStepMovementTest extends TestCase{
+    private int movement = 2;
 
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public OneStepMovementTest(String testName) {
+    public TwoStepMovementTest(String testName) {
         super(testName);
     }
 
@@ -21,7 +21,7 @@ public class OneStepMovementTest extends TestCase{
      * @return the suite of tests being tested
      */
     public static Test suite() {
-        return new TestSuite(OneStepMovementTest.class);
+        return new TestSuite(TwoStepMovementTest.class);
     }
 
     private void invalidPerpendicular(int currentPosition, int direction, int movement){
@@ -45,9 +45,9 @@ public class OneStepMovementTest extends TestCase{
     }
 
     /**
-     * Test one UP_LEFT movement from position 0 through 8
+     * Test two UP_LEFT movement from position 0 through 8
      */
-    public void test_One_UP_LEFT_from_0_to_8() {
+    public void test_Two_UP_LEFT_from_0_to_8() {
         int direction = Movement.UP_LEFT.getCode();
         invalidPerpendicular(0, direction, movement);
         invalidDiagonal(0, direction, movement);
@@ -58,25 +58,22 @@ public class OneStepMovementTest extends TestCase{
         invalidPerpendicular(3, direction, movement);
         invalidDiagonal(3, direction, movement);
         invalidPerpendicular(4, direction, movement);
-        Movement m4 = validDiagonal(4, direction, movement);
-        assertTrue(m4.getNewPosition() == 0);
+        invalidDiagonal(4, direction, movement);
         invalidPerpendicular(5, direction, movement);
-        Movement m5 = validDiagonal(5, direction, movement);
-        assertTrue(m5.getNewPosition() == 1);
+        invalidDiagonal(5, direction, movement);
         invalidPerpendicular(6, direction, movement);
         invalidDiagonal(6, direction, movement);
         invalidPerpendicular(7, direction, movement);
-        Movement m7 = validDiagonal(7, direction, movement);
-        assertTrue(m7.getNewPosition() == 3);
+        invalidDiagonal(7, direction, movement);
         invalidPerpendicular(8, direction, movement);
         Movement m8 = validDiagonal(8, direction, movement);
-        assertTrue(m8.getNewPosition() == 4);
+        assertTrue(m8.getNewPosition() == 0);
     }
 
     /**
-     * Test one UP movement from position 0 through 8
+     * Test two UP movement from position 0 through 8
      */
-    public void test_One_UP_from_0_to_8() {
+    public void test_Two_UP_from_0_to_8() {
         int direction = Movement.UP.getCode();
         invalidPerpendicular(0, direction, movement);
         invalidDiagonal(0, direction, movement);
@@ -84,30 +81,27 @@ public class OneStepMovementTest extends TestCase{
         invalidDiagonal(1, direction, movement);
         invalidPerpendicular(2, direction, movement);
         invalidDiagonal(2, direction, movement);
-        Movement m3 = validPerpendicular(3, direction, movement);
-        assertTrue(m3.getNewPosition() == 0);
+        invalidPerpendicular(3, direction, movement);
         invalidDiagonal(3, direction, movement);
-        Movement m4 = validPerpendicular(4, direction, movement);
-        assertTrue(m4.getNewPosition() == 1);
+        invalidPerpendicular(4, direction, movement);
         invalidDiagonal(4, direction, movement);
-        Movement m5 = validPerpendicular(5, direction, movement);
-        assertTrue(m5.getNewPosition() == 2);
+        invalidPerpendicular(5, direction, movement);
         invalidDiagonal(5, direction, movement);
         Movement m6 = validPerpendicular(6, direction, movement);
-        assertTrue(m6.getNewPosition() == 3);
+        assertTrue(m6.getNewPosition() == 0);
         invalidDiagonal(6, direction, movement);
         Movement m7 = validPerpendicular(7, direction, movement);
-        assertTrue(m7.getNewPosition() == 4);
+        assertTrue(m7.getNewPosition() == 1);
         invalidDiagonal(7, direction, movement);
         Movement m8 = validPerpendicular(8, direction, movement);
-        assertTrue(m8.getNewPosition() == 5);
+        assertTrue(m8.getNewPosition() == 2);
         invalidDiagonal(8, direction, movement);
     }
 
     /**
-     * Test one UP_RIGHT movement from position 0 through 8
+     * Test two UP_RIGHT movement from position 0 through 8
      */
-    public void test_One_UP_RIGHT_from_0_to_8() {
+    public void test_Two_UP_RIGHT_from_0_to_8() {
         int direction = Movement.UP_RIGHT.getCode();
         invalidPerpendicular(0, direction, movement);
         invalidDiagonal(0, direction, movement);
@@ -116,58 +110,52 @@ public class OneStepMovementTest extends TestCase{
         invalidPerpendicular(2, direction, movement);
         invalidDiagonal(2, direction, movement);
         invalidPerpendicular(3, direction, movement);
-        Movement m3 = validDiagonal(3, direction, movement);
-        assertTrue(m3.getNewPosition() == 1);
+        invalidDiagonal(3, direction, movement);
         invalidPerpendicular(4, direction, movement);
-        Movement m4 = validDiagonal(4, direction, movement);
-        assertTrue(m4.getNewPosition() == 2);
+        invalidDiagonal(4, direction, movement);
         invalidPerpendicular(5, direction, movement);
         invalidDiagonal(5, direction, movement);
         invalidPerpendicular(6, direction, movement);
         Movement m6 = validDiagonal(6, direction, movement);
-        assertTrue(m6.getNewPosition() == 4);
+        assertTrue(m6.getNewPosition() == 2);
         invalidPerpendicular(7, direction, movement);
-        Movement m7 = validDiagonal(7, direction, movement);
-        assertTrue(m7.getNewPosition() == 5);
+        invalidDiagonal(7, direction, movement);
         invalidPerpendicular(8, direction, movement);
         invalidDiagonal(8, direction, movement);
     }
 
     /**
-     * Test one LEFT movement from position 0 through 8
+     * Test two LEFT movement from position 0 through 8
      */
-    public void test_One_LEFT_from_0_to_8() {
+    public void test_Two_LEFT_from_0_to_8() {
         int direction = Movement.LEFT.getCode();
         invalidPerpendicular(0, direction, movement);
         invalidDiagonal(0, direction, movement);
-        Movement m1 = validPerpendicular(1, direction, movement);
-        assertTrue(m1.getNewPosition() == 0);
+        invalidPerpendicular(1, direction, movement);
         invalidDiagonal(1, direction, movement);
         Movement m2 = validPerpendicular(2, direction, movement);
-        assertTrue(m2.getNewPosition() == 1);
+        assertTrue(m2.getNewPosition() == 0);
         invalidDiagonal(2, direction, movement);
         invalidPerpendicular(3, direction, movement);
         invalidDiagonal(3, direction, movement);
-        Movement m4 = validPerpendicular(4, direction, movement);
-        assertTrue(m4.getNewPosition() == 3);
+        invalidPerpendicular(4, direction, movement);
         invalidDiagonal(4, direction, movement);
         Movement m5 = validPerpendicular(5, direction, movement);
-        assertTrue(m5.getNewPosition() == 4);
+        assertTrue(m5.getNewPosition() == 3);
         invalidDiagonal(5, direction, movement);
         invalidPerpendicular(6, direction, movement);
         invalidDiagonal(6, direction, movement);
-        Movement m7 = validPerpendicular(7, direction, movement);
-        assertTrue(m7.getNewPosition() == 6);
+        invalidPerpendicular(7, direction, movement);
         invalidDiagonal(7, direction, movement);
         Movement m8 = validPerpendicular(8, direction, movement);
-        assertTrue(m8.getNewPosition() == 7);
+        assertTrue(m8.getNewPosition() == 6);
         invalidDiagonal(8, direction, movement);
     }
 
     /**
-     * Test one STILL movement from position 0 through 8
+     * Test two STILL movement from position 0 through 8
      */
-    public void test_One_STILL_from_0_to_8() {
+    public void test_Two_STILL_from_0_to_8() {
         int direction = Movement.STILL.getCode();
         Movement m;
         m = validPerpendicular(0, direction, movement);
@@ -227,58 +215,26 @@ public class OneStepMovementTest extends TestCase{
     }
 
     /**
-     * Test one RIGHT movement from position 0 through 8
+     * Test two RIGHT movement from position 0 through 8
      */
-    public void test_One_RIGHT_from_0_to_8() {
+    public void test_Two_RIGHT_from_0_to_8() {
         int direction = Movement.RIGHT.getCode();
         Movement m0 = validPerpendicular(0, direction, movement);
-        assertTrue(m0.getNewPosition() == 1);
+        assertTrue(m0.getNewPosition() == 2);
         invalidDiagonal(0, direction, movement);
-        Movement m1 = validPerpendicular(1, direction, movement);
-        assertTrue(m1.getNewPosition() == 2);
+        invalidPerpendicular(1, direction, movement);
         invalidDiagonal(1, direction, movement);
         invalidPerpendicular(2, direction, movement);
         invalidDiagonal(2, direction, movement);
         Movement m3 = validPerpendicular(3, direction, movement);
-        assertTrue(m3.getNewPosition() == 4);
+        assertTrue(m3.getNewPosition() == 5);
         invalidDiagonal(3, direction, movement);
-        Movement m4 = validPerpendicular(4, direction, movement);
-        assertTrue(m4.getNewPosition() == 5);
+        invalidPerpendicular(4, direction, movement);
         invalidDiagonal(4, direction, movement);
         invalidPerpendicular(5, direction, movement);
         invalidDiagonal(5, direction, movement);
         Movement m6 = validPerpendicular(6, direction, movement);
-        assertTrue(m6.getNewPosition() == 7);
-        invalidDiagonal(6, direction, movement);
-        Movement m7 = validPerpendicular(7, direction, movement);
-        assertTrue(m7.getNewPosition() == 8);
-        invalidDiagonal(7, direction, movement);
-        invalidPerpendicular(8, direction, movement);
-        invalidDiagonal(8, direction, movement);
-    }
-
-    /**
-     * Test one DOWN_LEFT movement from position 0 through 8
-     */
-    public void test_One_DOWN_LEFT_from_0_to_8() {
-        int direction = Movement.DOWN_LEFT.getCode();
-        invalidPerpendicular(0, direction, movement);
-        invalidDiagonal(0, direction, movement);
-        invalidPerpendicular(1, direction, movement);
-        Movement m1 = validDiagonal(1, direction, movement);
-        assertTrue(m1.getNewPosition() == 3);
-        invalidPerpendicular(2, direction, movement);
-        Movement m2 = validDiagonal(2, direction, movement);
-        assertTrue(m2.getNewPosition() == 4);
-        invalidPerpendicular(3, direction, movement);
-        invalidDiagonal(3, direction, movement);
-        invalidPerpendicular(4, direction, movement);
-        Movement m4 = validDiagonal(4, direction, movement);
-        assertTrue(m4.getNewPosition() == 6);
-        invalidPerpendicular(5, direction, movement);
-        Movement m5 = validDiagonal(5, direction, movement);
-        assertTrue(m5.getNewPosition() == 7);
-        invalidPerpendicular(6, direction, movement);
+        assertTrue(m6.getNewPosition() == 8);
         invalidDiagonal(6, direction, movement);
         invalidPerpendicular(7, direction, movement);
         invalidDiagonal(7, direction, movement);
@@ -287,27 +243,22 @@ public class OneStepMovementTest extends TestCase{
     }
 
     /**
-     * Test one DOWN movement from position 0 through 8
+     * Test two DOWN_LEFT movement from position 0 through 8
      */
-    public void test_One_DOWN_from_0_to_8() {
-        int direction = Movement.DOWN.getCode();
-        Movement m0 = validPerpendicular(0, direction, movement);
-        assertTrue(m0.getNewPosition() == 3);
+    public void test_Two_DOWN_LEFT_from_0_to_8() {
+        int direction = Movement.DOWN_LEFT.getCode();
+        invalidPerpendicular(0, direction, movement);
         invalidDiagonal(0, direction, movement);
-        Movement m1 = validPerpendicular(1, direction, movement);
-        assertTrue(m1.getNewPosition() == 4);
+        invalidPerpendicular(1, direction, movement);
         invalidDiagonal(1, direction, movement);
-        Movement m2 = validPerpendicular(2, direction, movement);
-        assertTrue(m2.getNewPosition() == 5);
-        invalidDiagonal(2, direction, movement);
-        Movement m3 = validPerpendicular(3, direction, movement);
-        assertTrue(m3.getNewPosition() == 6);
+        invalidPerpendicular(2, direction, movement);
+        Movement m2 = validDiagonal(2, direction, movement);
+        assertTrue(m2.getNewPosition() == 6);
+        invalidPerpendicular(3, direction, movement);
         invalidDiagonal(3, direction, movement);
-        Movement m4 = validPerpendicular(4, direction, movement);
-        assertTrue(m4.getNewPosition() == 7);
+        invalidPerpendicular(4, direction, movement);
         invalidDiagonal(4, direction, movement);
-        Movement m5 = validPerpendicular(5, direction, movement);
-        assertTrue(m5.getNewPosition() == 8);
+        invalidPerpendicular(5, direction, movement);
         invalidDiagonal(5, direction, movement);
         invalidPerpendicular(6, direction, movement);
         invalidDiagonal(6, direction, movement);
@@ -318,24 +269,49 @@ public class OneStepMovementTest extends TestCase{
     }
 
     /**
-     * Test one DOWN_RIGHT movement from position 0 through 8
+     * Test two DOWN movement from position 0 through 8
      */
-    public void test_One_DOWN_RIGHT_from_0_to_8() {
+    public void test_Two_DOWN_from_0_to_8() {
+        int direction = Movement.DOWN.getCode();
+        Movement m0 = validPerpendicular(0, direction, movement);
+        assertTrue(m0.getNewPosition() == 6);
+        invalidDiagonal(0, direction, movement);
+        Movement m1 = validPerpendicular(1, direction, movement);
+        assertTrue(m1.getNewPosition() == 7);
+        invalidDiagonal(1, direction, movement);
+        Movement m2 = validPerpendicular(2, direction, movement);
+        assertTrue(m2.getNewPosition() == 8);
+        invalidDiagonal(2, direction, movement);
+        invalidPerpendicular(3, direction, movement);
+        invalidDiagonal(3, direction, movement);
+        invalidPerpendicular(4, direction, movement);
+        invalidDiagonal(4, direction, movement);
+        invalidPerpendicular(5, direction, movement);
+        invalidDiagonal(5, direction, movement);
+        invalidPerpendicular(6, direction, movement);
+        invalidDiagonal(6, direction, movement);
+        invalidPerpendicular(7, direction, movement);
+        invalidDiagonal(7, direction, movement);
+        invalidPerpendicular(8, direction, movement);
+        invalidDiagonal(8, direction, movement);
+    }
+
+    /**
+     * Test two DOWN_RIGHT movement from position 0 through 8
+     */
+    public void test_Two_DOWN_RIGHT_from_0_to_8() {
         int direction = Movement.DOWN_RIGHT.getCode();
         invalidPerpendicular(0, direction, movement);
         Movement m0 = validDiagonal(0, direction, movement);
-        assertTrue(m0.getNewPosition() == 4);
+        assertTrue(m0.getNewPosition() == 8);
         invalidPerpendicular(1, direction, movement);
-        Movement m1 = validDiagonal(1, direction, movement);
-        assertTrue(m1.getNewPosition() == 5);
+        invalidDiagonal(1, direction, movement);
         invalidPerpendicular(2, direction, movement);
         invalidDiagonal(2, direction, movement);
         invalidPerpendicular(3, direction, movement);
-        Movement m3 = validDiagonal(3, direction, movement);
-        assertTrue(m3.getNewPosition() == 7);
+        invalidDiagonal(3, direction, movement);
         invalidPerpendicular(4, direction, movement);
-        Movement m4 = validDiagonal(4, direction, movement);
-        assertTrue(m4.getNewPosition() == 8);
+        invalidDiagonal(4, direction, movement);
         invalidPerpendicular(5, direction, movement);
         invalidDiagonal(5, direction, movement);
         invalidPerpendicular(6, direction, movement);
