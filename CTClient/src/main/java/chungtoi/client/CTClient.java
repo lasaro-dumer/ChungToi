33a656002d1ed9782d1c37e7d412c0083f94b71f
Lasaro Dumer <lasaro.dumer@gmail.com>
2017-11-18 11:24:49 -0200

    Client: accepting folder or list, also needs full file path now for .in

--- CTClient/src/main/java/chungtoi/client/CTClient.java
+++ CTClient/src/main/java/chungtoi/client/CTClient.java
@@ -10,6 +10,11 @@ import chungtoi.client.proxy.ChungToi;
 import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Files;
 
 /**
  *
@@ -26,9 +31,43 @@ public class CTClient {
                 client.startGame();
             }
             else{
-                for (int i = 0; i < args.length; i++) {
-                    BatchClient bc = new BatchClient(chungToi);
-                    bc.executaTeste(args[i]);
+                String path = args[0];
+                Path file = new File(path).toPath();
+
+                boolean exists =      Files.exists(file);        // Check if the file exists
+                boolean isDirectory = Files.isDirectory(file);   // Check if it's a directory
+                boolean isFile =      Files.isRegularFile(file); // Check if it's a regular file
+                if(exists){
+                    List<Path> filesToProcess = new ArrayList<>();
+                    if(isDirectory){
+                        File dir = new File(path);
+                        File[] files = dir.listFiles();
+                        for (File f : files) {
+                            Path filePath = f.toPath();
+                            if(filePath.toString().endsWith(".in") || filePath.toString().endsWith(".lst"))
+                                filesToProcess.add(filePath);
+                        }
+                        System.out.println("Found files "+files.length+" in folder "+path+", and "+filesToProcess.size()+" will be processed");
+                    }
+                    else{
+                        filesToProcess.add(file);
+                    }
+                    for (Path f : filesToProcess) {
+                        if(f.toString().endsWith(".in")){
+                            String fileRad = f.toString();
+                            fileRad = fileRad.substring(0, fileRad.lastIndexOf("."));
+                            System.out.println("fileRad: " + fileRad);
+                            BatchClient bc = new BatchClient(chungToi);
+                            bc.executaTeste(fileRad);
+                        } else if (f.toString().endsWith(".lst")) {
+                            System.out.println("This is a test list");
+                        } else {
+                            System.out.println("Invalid file ["+f.toString()+"] skipped.");
+                        }
+                    }
+                }
+                else{
+                    System.out.println("Path ["+path+"] is invalid.");
                 }
             }
         } catch (Exception e) {
